120d1b5f27b8f35f90c3b7cbb5e2445042425246
Todd C <toc5012@gmail.com>
2016-01-05 16:50:10 -0500

    Update controllers.js

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -49,8 +49,8 @@ angular.module('weatherApp')
 
       		var details = $scope.details1;
       		var coords = details.geometry;
-	      		$scope.lat = coords.location[0];
-	      		$scope.lon = coords.location[1];
+	      		$scope.lat = coords.viewport.N.N;
+	      		$scope.lon = coords.viewport.j.N;
       		
       		//retrieving the current weather 
       		weatherData.getWeather($scope.lat,$scope.lon).then(function(result) {
